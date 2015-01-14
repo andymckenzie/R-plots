767e81475470b82d564c365689fe57b6a87b8834
Andy McKenzie <amckenz@gmail.com>
2015-01-14 14:21:33 -0500

    changes

--- tomasetti_data/analyze_tom.R
+++ tomasetti_data/analyze_tom.R
@@ -8,3 +8,5 @@ tom_data[tom_data$Lifetime_Incidence > 0.03, ]
 qplot(tom_data$Lifetime_Incidence, as.numeric(tom_data$Total_SC_Divions_Lifetime_Tissue), xlab = "Lifetime Cancer Incidence", ylab = "Total SC Divisions Lifetime") + theme_bw()
 
 qplot(log(tom_data$Lifetime_Incidence), log(as.numeric(tom_data$Total_SC_Divions_Lifetime_Tissue)), xlab = "Lifetime Cancer Incidence", ylab = "Total SC Divisions Lifetime") + theme_bw()
+
+#testing more changes
