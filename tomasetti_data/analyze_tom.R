73cbec79cdd4e9821c3bb4a2c7b268e818837e5a
Andy McKenzie <amckenz@gmail.com>
2015-01-14 15:49:25 -0500

    added the ERS portion

--- tomasetti_data/analyze_tom.R
+++ tomasetti_data/analyze_tom.R
@@ -1,7 +1,11 @@
 tom_data = read.table(file = "/Users/amckenz/Dropbox/zhang/journal_club/tomasetti_data.txt", 
 	header = TRUE, sep = " ", row.names = NULL, fill = TRUE) 
 
-cor(tom_data$Lifetime_Incidence, as.numeric(tom_data$Total_SC_Divions_Lifetime_Tissue))
+normal_corr = cor(tom_data$Lifetime_Incidence, as.numeric(tom_data$Total_SC_Divions_Lifetime_Tissue))
+ 
+log_corr = cor(log(tom_data$Lifetime_Incidence), log(as.numeric(tom_data$Total_SC_Divions_Lifetime_Tissue))) 
+#= 0.8039286, reproducing the result from the paper 
+#square this to get r^2
  
 tom_data[tom_data$Lifetime_Incidence > 0.03, ] 
 
@@ -15,6 +19,13 @@ qplot(log(tom_data$Lifetime_Incidence),
 	log(as.numeric(tom_data$Total_SC_Divions_Lifetime_Tissue)), 
 	xlab = "Lifetime Cancer Incidence", 
 	ylab = "Total SC Divisions Lifetime") + 
-	theme_bw() + stat_smooth(method="lm",se=TRUE)
+	theme_bw() + stat_smooth(method="lm",se=TRUE) 
+	
+tom_data$ERS = log(tom_data$Lifetime_Incidence, 10) *
+	log(tom_data$Total_SC_Divions_Lifetime_Tissue, 10) + 18.49 
 
-#testing more changes
+kmean_tom = kmeans(tom_data$ERS, 2)
+
+tom_data$cluster_status = kmean_tom$cluster
+
+plot(sort(tom_data$ERS))
