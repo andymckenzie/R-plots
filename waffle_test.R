3dbc60b1220a6b703a0a0ac165763f2219efba0f
andymckenzie <amckenz@gmail.com>
2015-05-02 19:17:36 -0400

    learned how to make a waffle plot

---
+++ waffle_test.R
@@ -0,0 +1,10 @@
+library(waffle)
+#this is what a success rate of 99.6% refers to 
+#source: http://alzres.com/content/6/4/37
+clinical_trial_failures = 249
+clinical_trial_successes = 1
+alzheimers_trials = c('Failures' = 249, 
+	'Successes' = 1)
+waffle(alzheimers_trials, rows=12, size=1, 
+       title="Trials for Alzheimer's Disease, 2002 - 2012")
+	  
