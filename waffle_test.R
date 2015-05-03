b8b54ac44d28f7ccb85bdb503685057ae0287bfc
Andy McKenzie <amckenz@gmail.com>
2015-05-03 00:04:34 -0400

    Update waffle_test.R

--- waffle_test.R
+++ waffle_test.R
@@ -1,8 +1,6 @@
 library(waffle)
 #this is what a success rate of 99.6% refers to 
 #source: http://alzres.com/content/6/4/37
-clinical_trial_failures = 249
-clinical_trial_successes = 1
 alzheimers_trials = c('Failures' = 249, 
 	'Successes' = 1)
 waffle(alzheimers_trials, rows=12, size=1, 
