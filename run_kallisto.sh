4a5faca6431ceec8addf3065859f02084b5aa78a
Andy McKenzie <amckenz@gmail.com>
2015-05-12 15:51:11 -0400

    adding some files to try running kallisto (which work)

---
+++ run_kallisto.sh
@@ -0,0 +1,25 @@
+#path to where the SRA bin folder is located; could eliminate by making SRAT globally executable 
+PATH_SRA_DIR="your_path_here/"
+
+#go to where your kallisto test directory is found, if necessary
+echo "cd your_path_here"
+
+#load the config file
+source test_kallisto/config_SRP006900.file
+
+#download the data 
+for i in "${arr[@]}"
+do
+   "$PATH_SRA_DIR"sratoolkit.2.5.0-mac64/bin/fastq-dump --accession $i --outdir test_kallisto
+done
+
+#create the intranscripts index file 
+kallisto index -i transcripts.idx transcripts.fasta.gz
+
+#create output directories, and then run kallisto for each of the runs
+for i in "${arr[@]}"
+do
+	mkdir output_"$i"
+	kallisto quant -i transcripts.idx -o output_"$i" -l "$AVG_READ_LENGTH" "$i"".fastq"
+done
+
