03b52f63c60193766bab4e30cfca8ba2a1a3b08a
andymckenzie <amckenz@gmail.com>
2015-05-30 10:56:15 -1000

    basic class learning in c++ and calling them via a header fxn

---
+++ learn_cpp/animal.cpp
@@ -0,0 +1,18 @@
+#include "animalclass.h"
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+	
+	Animal animal_one;
+	Llama llama_one; 
+	
+    //cout << "Animal says : " << 
+	animal_one.move(); 
+	
+    //cout << "Llama says : " <<  ; 
+	llama_one.eat();
+	
+	return 0; 
+}
