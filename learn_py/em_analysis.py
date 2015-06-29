85330eaf5a6ed59cde33f3cdc6202dbb47288f60
andymckenzie <amckenz@gmail.com>
2015-06-28 21:09:25 -0400

    next step

--- learn_py/em_analysis.py
+++ learn_py/em_analysis.py
@@ -5,14 +5,49 @@ from matplotlib import pyplot as plt
 em = cv2.imread("em_data/em_peters.png") 
 em = cv2.cvtColor(em,cv2.COLOR_BGR2GRAY)
 em = cv2.resize(em, (0,0), fx = 0.6, fy = 0.6)
-#
-ret, thresh = cv2.threshold(thresh,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
 
-#
+#############################
+#Otsu method
+ret, thresh = cv2.threshold(em,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+
+#cv2.imshow('test', thresh)
+
+##############################
+#Canny edge detection 
 edges = cv2.Canny(em,100,200)
-plt.subplot(121),plt.imshow(em,cmap = 'gray')
-plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(edges,cmap = 'gray')
-plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
 
-plt.show()
+#cv2.imshow('test', edges)
+
+##############################
+# blob detection
+#http://stackoverflow.com/questions/8076889/tutorial-on-opencv-simpleblobdetector
+#http://answers.opencv.org/question/38218/module-object-has-no-attribute-simpleblobdetector/
+
+params = cv2.SimpleBlobDetector_Params()
+
+# Change thresholds
+params.minThreshold = 10
+params.maxThreshold = 200
+
+# Filter by Area.
+params.filterByArea = True
+params.minArea = 50
+
+# Filter by Circularity
+params.filterByCircularity = True
+params.minCircularity = 0.1
+
+# Filter by Inertia
+params.filterByInertia = True
+params.minInertiaRatio = 0.01
+
+detector = cv2.SimpleBlobDetector_create(params)
+ 
+keypoints = detector.detect(em)
+ 
+# Draw detected blobs as red circles.
+# cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
+im_with_keypoints = cv2.drawKeypoints(em, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+ 
+# Show keypoints
+cv2.imshow("Keypoints", im_with_keypoints)
