519d37a18d6ff12b3e8ac49dc0b871953fbe8209
Andy McKenzie <amckenz@gmail.com>
2015-12-16 13:11:26 -0500

    updating this a bit

--- BDA/bayes_autoregressive.R
+++ BDA/bayes_autoregressive.R
@@ -5,11 +5,14 @@
 library(rjags)
 library(reshape2)
 
+ESPN = TRUE
+fleaflicker = FALSE
+
 #need to run in the BDA folder
 if(ESPN){
-  data = read.csv("FFStats.csv")
+  data = read.csv("ffdata_week14.csv")
   data$ID = 1:length(data$Owner)
-  datam = data[ , c("ID", paste0("W", 1:12, "F"))]
+  datam = data[ , c("ID", paste0("W", 1:14, "F"))]
   dataa = melt(datam, id = "ID", value.name = "Points")
 }
 
@@ -23,16 +26,20 @@ if(fleaflicker){
 #load the data for each team
 
 #to predict next week
+IDs = c(as.integer(dataa$ID), 1:length(unique(dataa$ID)))
+y = c(dataa$Points, rep(NA, length(unique(dataa$ID))))
+x = c(as.integer(dataa$variable), rep(max(as.integer(dataa$variable)),
+  length(unique(dataa$ID))))
 
 ########################
 # preparing data for JAGS
 #also make a team variable so that we can estimate this for each group
 ffdata_dat = list(
-  id = as.integer(dataa$ID),
-	y = dataa$Points,
-	x = as.integer(dataa$variable), #week
-	Nteams = length(unique(dataa$ID)),
-  Ndata = nrow(dataa))
+  id = IDs,
+	y = y,
+	x = x, #week
+	Nteams = length(unique(IDs)),
+  Ndata = length(y))
 
 model_string = " model {
     for( teamIndx in 1 : Nteams){
@@ -62,12 +69,49 @@ ffdata_mod <- jags.model( textConnection(model_string),
 update(ffdata_mod, 500) # burn-in
 
 ffdata_res <- coda.samples( ffdata_mod,
-	var = c("ar1", "beta0", "beta1", "tau", "nu"),
-	n.iter = 500,
+	var = c("ar1", "beta0", "beta1", "tau", "nu", "y"),
+	n.iter = 2000,
 	thin = 5 )
 
 summary(ffdata_res)
-plot(ffdata_res)
+# plot(ffdata_res)
+
+emmett_mcmc <- unlist(ffdata_res[ , "y[141]"])
+mike_mcmc <- unlist(ffdata_res[ , "y[147]"])
+eddy_mcmc <- unlist(ffdata_res[ , "y[143]"])
+jnay_mcmc <- unlist(ffdata_res[ , "y[149]"])
+
+sum(emmett_mcmc - mike_mcmc > 0)/length(eddy_mcmc)
+mean(emmett_mcmc - mike_mcmc)
+
+sum(eddy_mcmc - jnay_mcmc > 0)/length(eddy_mcmc)
+mean(eddy_mcmc - jnay_mcmc)
+
+ranjan_mcmc <- unlist(ffdata_res[ , "y[141]"])
+max_mcmc <- unlist(ffdata_res[ , "y[147]"])
+eddy_mcmc <- unlist(ffdata_res[ , "y[143]"])
+jnay_mcmc <- unlist(ffdata_res[ , "y[149]"])
+
+sum(ranjan_mcmc - max_mcmc > 0)/length(eddy_mcmc)
+mean(ranjan_mcmc - max_mcmc)
+
+sum(eddy_mcmc - jnay_mcmc > 0)/length(eddy_mcmc)
+mean(eddy_mcmc - jnay_mcmc)
+
+predict_matchup <- function(var1, var2){
+
+  team1 = unlist(ffdata_res[ , var1])
+  team2 = unlist(ffdata_res[ , var2])
+
+  prob_team1 = sum(team1 - team2 > 0)/length(team1)
+  mean_diff_team1 = sum(team1 - team2 > 0)/length(team1)
+
+  print(prob_team1)
+  print(mean_diff_team1)
+
+}
+
+
 
 
 # }
