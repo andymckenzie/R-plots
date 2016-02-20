d9a09a7a29ee6a8f19f5648b2623ab01a7d795e9
Andy McKenzie <amckenz@gmail.com>
2016-02-19 22:31:15 -0500

    this works for the water blocks

--- water_blocks.R
+++ water_blocks.R
@@ -1,24 +1,53 @@
 #http://stackoverflow.com/questions/24414700/amazon-water-collected-between-towers
-#want to start on this, but haven't made much headway
-#trying to avoid a brute force (for loops) but idk why really
+#alright pretty naive/brute force solution but whatever it works
 
-input = c(1,5,3,7,2)
+#You are given an input array whose each element represents the height of a line towers.
+#The width of every tower is 1. It starts raining. How much water is collected between the towers?
+
+input1 = c(1,5,3,7,2)
 input2 = c(5,3,7,2,6,4,5,9,1,2)
 
-get_water_length <- function(tow){
+input = input2
 
-  #get the
-  block_pillars_indx = which((tow > tow[-1])[1:(length(tow)-1)])
-  block_pillar_heights = tow[block_pillars_indx]
+#find ones that are higher than the previous one
+higher_than_previous = rep(FALSE, length(input2))
+peaks = rep(FALSE, length(input2))
 
-  #for each block pillar pair, find which one is higher
-  highest_block_indx = which((block_pillar_heights >
-    block_pillar_heights[-1])[1:(length(block_pillar_heights)-1)])
-
-  blocks_first_indx = block_pillars_indx[-length(block_pillars_indx)]
-  blocks_second_indx = block_pillars_indx[-1]
-
-
+higher_than_previous[1] = TRUE
+peaks[1] = TRUE
+for(i in 1:length(input)){
+  if(i == 1){
+    next
+  }
+  if(input[i]>input[i-1]){
+    higher_than_previous[i] = TRUE
+    peaks[i] = TRUE
+  }
+  if(higher_than_previous[i] == TRUE & higher_than_previous[i-1] == TRUE){
+    peaks[i-1] = FALSE
+  }
 }
 
-cbind(tow, c(0, tow), tow > c(0, tow), diff(sign(input2)))
+#for each peak pair, find which one is higher
+peaks_indices = which(peaks)
+peak_heights = input[peaks_indices]
+peak_pairs_heights_first = peak_heights[-length(peak_heights)]
+peak_pairs_heights_second = peak_heights[-1]
+peak_pairs_higher = peak_pairs_heights_first > peak_pairs_heights_second
+peak_pairs_lower_height = ifelse(peak_pairs_higher, peak_pairs_heights_second, peak_pairs_heights_first)
+
+#for the space within each of the peak pairs,
+total_water = 0
+peak_pair_water = numeric(length(peak_pairs_lower_height))
+for(i in 1:length(peak_pairs_lower_height)){
+  #identify the indices of the peak to loop over
+  for(j in (peaks_indices[i]+1):(peaks_indices[i+1]-1)){
+    print('new iteration')
+    print(input[j])
+    print(peak_pairs_lower_height[i])
+    peak_pair_water[i] = peak_pair_water[i] + (peak_pairs_lower_height[i] - input[j])
+  }
+  total_water = total_water + peak_pair_water[i]
+}
+print(peak_pair_water)
+print(total_water)
