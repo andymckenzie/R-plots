70c7b73320623c386606fc9dde6889235740a7e5
andymckenzie <amckenz@gmail.com>
2016-02-20 11:42:08 -0500

    learned something new about how to use regexp to remove trailing whitespace and character matching

---
+++ parse_inventory.R
@@ -0,0 +1,4 @@
+inventory = read.delim("/Users/amckenz/Desktop/inventory.tsv", quote = "")
+#http://stackoverflow.com/questions/10502787/removing-trailing-spaces-with-gsub-in-r
+inventory$Category_clean = gsub("[[:space:]]*$", "", inventory$Category)
+table(inventory$Category_clean) 
