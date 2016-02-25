044905ef4356f7988de12992f6e7d362bd906201
andymckenzie <amckenz@gmail.com>
2016-02-24 22:23:40 -0500

    plan to add melatonin analysis

---
+++ ana_melatonin.R
@@ -0,0 +1,14 @@
+melatonin = read.delim("Melatonin_dose_response.tsv")
+
+#for each category... will merge the pill number with the category, to get the category for each
+#then, will find the means and standard deviations for each of the following variables:
+
+#estimated placebo probability
+#how helpful getting to sleep
+#how helpful getting to sleep
+#quality of sleep rating
+#how much slept at night
+#how long it took to get to sleep
+
+#+/- adjusting for or throwing out based on blue blockers, lack of alcohol, or extenuating circs types of things
+#will remove the two times i said "throw out" in the extenuating circs for sure
